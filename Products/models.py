0e0c82d95cd52b9561e9b1ac65846f3c0fd45693
abdelrahman <abdo.aly.k.m@gmail.com>
2024-05-14 00:29:16 +0300

    making model obj show with name

--- Products/models.py
+++ Products/models.py
@@ -8,3 +8,6 @@ class product_item(models.Model):
     price = models.DecimalField(max_digits=5,decimal_places=2)
     image = models.ImageField(upload_to='Product_photos/%y/%m/%d')
     status = models.BooleanField(default=True)
+    
+    def __str__ (self):
+        return self.name
