40556919bd0bbf44ebb123a992bb0ebbc0657d1b
abdelrahman <abdo.aly.k.m@gmail.com>
2024-05-14 17:58:33 +0300

    adding static some templetes

--- Pages/urls.py
+++ Pages/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('log_in', views.log_in, name='log_in'),
     path('sign_up', views.sign_up, name='sign_up'),
     path('item', views.item, name='item'),
+    path('search', views.search, name='search'),
 ]
--- Pages/views.py
+++ Pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 #from django.http.response import HttpResponse
 # Create your views here.
 
+
+
 def index(request):
     return render(request,'index.html')
 
@@ -28,3 +30,8 @@ def sign_up(request):
 
 def products(request):
     return render(request,'products.html')
+
+
+
+def search(request):
+    return render(request,'search.html')
